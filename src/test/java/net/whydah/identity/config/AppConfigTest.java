036ab44850c7f55899f7588c4c53e62bca6c1513
Erik Drolshammer <erik-dev@fjas.no>
2014-01-21 19:58:00 +0100

    Change groupid, version and package structure.

---
+++ src/test/java/net/whydah/identity/config/AppConfigTest.java
@@ -0,0 +1,23 @@
+package net.whydah.identity.config;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppConfigTest {
+    @BeforeClass
+    public static void init() {
+        System.setProperty(ApplicationMode.IAM_MODE_KEY, ApplicationMode.TEST);
+        System.setProperty(AppConfig.IAM_CONFIG_KEY, "src/test/testconfig.properties");
+    }
+
+    @Test
+    public void readProperties() throws IOException {
+        AppConfig appConfig = new AppConfig();
+        assertEquals("puh", appConfig.getProperty("nalle")); //fra testconfig
+        assertEquals("9992", appConfig.getProperty("service.port")); //fra useradminservice.TEST.properties
+    }
+}
