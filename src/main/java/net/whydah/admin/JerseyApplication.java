48e04b26a54adb0fc617cf8dfbf696fb90edc16e
Bard Lind <bard.lind@tomra.com>
2014-04-10 08:00:15 +0200

    Startup failes.

---
+++ src/main/java/net/whydah/admin/JerseyApplication.java
@@ -0,0 +1,23 @@
+package net.whydah.admin;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * @author <a href="mailto:erik-dev@fjas.no">Erik Drolshammer</a>
+ */
+@ApplicationPath("overriddenByWebXml")
+public class JerseyApplication extends ResourceConfig {
+    private static final Logger log = LoggerFactory.getLogger(JerseyApplication.class);
+
+    public JerseyApplication() {
+        //https://java.net/jira/browse/JERSEY-2175
+        ResourceConfig resourceConfig = packages("com.tomra.receipt.admin", "com.tomra.receipt.audit");
+
+        //resourceConfig.register(MultiPartFeature.class);
+        log.debug(this.getClass().getSimpleName() + " started!");
+    }
+}
