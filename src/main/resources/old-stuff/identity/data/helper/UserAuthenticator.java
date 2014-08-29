7fffa641ae12ca0a7865bec10857cfd000bcba67
Totto <totto@totto.org>
2014-08-29 13:02:51 +0200

    Cleanup

--- src/main/resources/old-stuff/identity/data/helper/UserAuthenticator.java
+++
@@ -1,9 +0,0 @@
-package net.whydah.identity.data.helper;
-
-import net.whydah.identity.data.UserToken;
-
-public interface UserAuthenticator {
-    public UserToken logonUser(String appTokenXml, String userCredentialXml);
-
-    UserToken createAndLogonUser(String appTokenXml, String userCredentialXml, String fbUserXml);
-}
