16593d255bb31635667cbc15b6a339a4d9339255
Bard Lind <bard.lind@tomra.com>
2014-12-11 13:50:32 +0100

    Start woring on global system integrator verification.

---
+++ src/test/java/net/whydah/admin/VerifyUserAdminServiceMain.java
@@ -0,0 +1,17 @@
+package net.whydah.admin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Verify that every interface of UserAdminService respond in a propper way.
+ * See https://code.google.com/p/rest-assured/wiki/GettingStarted
+ * @author <a href="bard.lind@gmail.com">Bard Lind</a>
+ */
+public class VerifyUserAdminServiceMain {
+    private static final Logger log = LoggerFactory.getLogger(VerifyUserAdminServiceMain.class);
+
+    public static void main(String[] args) {
+        VerifyUserAdminServiceMain verificator = new VerifyUserAdminServiceMain();
+    }
+}
