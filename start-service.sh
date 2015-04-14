85419b4d51b5fa1f9d4627c75cc13c588fe4d1db
Thor Henning Hetland <totto@totto.org>
2015-04-14 16:20:16 +0200

    parametrized

--- start-service.sh
+++ start-service.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
-nohup /usr/bin/java -DIAM_MODE=PROD -DIAM_CONFIG=/home/UserAdminService/useradminservice.PROD.properties -jar /home/UserAdminService/UserAdminService.jar &
+#  If IAM_MODE not set, use PROD
+if [ -z "$IAM_MODE" ]
+  IAM_MODE=PROD
+fi
+
+# If IAM_CONFOG not set, use PROD properties from local directory
+if [ -z "$IAM_CONFIG" ]
+  IAM_CONFIG=useradminservice.PROD.properties
+fi
+
+# If Version is from source, find the artifact
+if [ $Version = "FROM_SOURCE" ]; then 
+    # Find the bult artifact
+    Version=$(find target/* -name *.jar | grep SNAPSHOT | grep -v original | grep -v lib)
+else
+    Version=UserAdminService.jar
+fi
+
 
 
