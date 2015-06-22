e9c6a60fff76ae4e647d24369f33a876d630d0ed
baardl <bard.lind@gmail.com>
2015-06-22 14:46:21 +0200

    Reading RoleRepresentations from json.

--- src/main/java/net/whydah/admin/user/uib/RoleRepresentationMapper.java
+++ src/main/java/net/whydah/admin/user/uib/RoleRepresentationMapper.java
@@ -3,19 +3,22 @@ package net.whydah.admin.user.uib;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by baardl on 22.06.15.
  */
 public class RoleRepresentationMapper {
 
-    public static RoleRepresentation fromJson(String roleJson) {
-        RoleRepresentation roleRepresentation = null;
+    public static List<RoleRepresentation> fromJson(String roleJson) {
+        List<RoleRepresentation> roles = null;
         try {
             ObjectMapper mapper = new ObjectMapper();
-            roleRepresentation = mapper.readValue(roleJson, RoleRepresentation.class);
+            roles= mapper.readValue(roleJson, new TypeReference<List<RoleRepresentation>>() {
+            });
 
         } catch (JsonMappingException e) {
             throw new IllegalArgumentException("Error mapping json for " + roleJson, e);
@@ -24,6 +27,6 @@ public class RoleRepresentationMapper {
         } catch (IOException e) {
             throw new IllegalArgumentException("Error reading json for " + roleJson, e);
         }
-        return roleRepresentation;
+        return roles;
     }
 }
--- src/test/java/net/whydah/admin/user/uib/RoleRepresentationMapperTest.java
+++ src/test/java/net/whydah/admin/user/uib/RoleRepresentationMapperTest.java
@@ -2,6 +2,11 @@ package net.whydah.admin.user.uib;
 
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
 /**
  * Created by baardl on 22.06.15.
  */
@@ -10,10 +15,13 @@ public class RoleRepresentationMapperTest {
     @Test
     public void testFromJson() throws Exception {
 
-        RoleRepresentation role = RoleRepresentation.fromJson(roleJson);
+        List<RoleRepresentation> role = RoleRepresentationMapper.fromJson(roleJson);
+        assertNotNull(role);
+        assertEquals(role.size(),1);
+
     }
 
-    private String roleJson = //"[\n" +
+    private String roleJson = "[\n" +
             "    {\n" +
             "        \"roleId\": \"8bafa279-1615-4833-869e-106daaefa797\",\n" +
             "        \"uid\": \"e365cccc-dc2b-4c79-a79c-d0ae3b79a45d\",\n" +
@@ -22,6 +30,6 @@ public class RoleRepresentationMapperTest {
             "        \"applicationRoleValue\": \"true\",\n" +
             "        \"applicationName\": \"DomainConfig\",\n" +
             "        \"organizationName\": \"testOrg\"\n" +
-            "    }\n";// +
-//            "]";
+            "    }\n" +
+            "]";
 }
