8f50bd57b1dfed96308b6a44ab4735346d5a7773
Sherriff <erik-dev@fjas.no>
2015-07-01 14:39:21 +0200

    #26 - simple proxy for UB application CRUD endpoint. Note! No authentication checks...

--- src/main/java/net/whydah/admin/application/ApplicationResource.java
+++ src/main/java/net/whydah/admin/application/ApplicationResource.java
@@ -1,40 +1,107 @@
 package net.whydah.admin.application;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import net.whydah.admin.config.AppConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 
 /**
+ *
+ * Proxy in front of UIB application CRUD endpoint.
+ *
  * @author <a href="bard.lind@gmail.com">Bard Lind</a>
+ * @author <a href="mailto:erik-dev@fjas.no">Erik Drolshammer</a>
  */
 @Path("/{applicationtokenid}/{userTokenId}/application")
 @Component
 public class ApplicationResource {
     private static final Logger log = LoggerFactory.getLogger(ApplicationResource.class);
-    ApplicationService applicationService;
-    ObjectMapper mapper = new ObjectMapper();
+
+    private static final String APPLICATION_PATH = "application";
+    private final WebTarget uib;
 
 
     @Autowired
-    public ApplicationResource(ApplicationService applicationService) {
-        this.applicationService = applicationService;
+    public ApplicationResource(AppConfig appConfig) {
+        Client client = ClientBuilder.newClient();
+        String uibUrl = appConfig.getProperty("useridentitybackend");
+        log.info("Connection to UserIdentityBackend on {}" , uibUrl);
+        uib = client.target(uibUrl);
     }
 
-    /**
-     * TODO: Enhance due to https://github.com/Cantara/Whydah-UserAdminService/issues/20
-     * Create a new applcation from json
-     * Add default
-     *
-     * @param applicationXml json representing an Application
-     * @return Application
-     */
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createApplication(@PathParam("applicationtokenid") String applicationTokenId,
+                                      @PathParam("userTokenId") String userTokenId,
+                                      String applicationJson)  {
+        log.trace("create is called with applicationJson={}", applicationJson);
+
+        WebTarget webResource = uib.path(applicationTokenId).path(userTokenId).path(APPLICATION_PATH);
+        Response responseFromUib = webResource.request(MediaType.APPLICATION_JSON).post(Entity.entity(applicationJson, MediaType.APPLICATION_JSON));
+        return copyResponse(responseFromUib);
+    }
+
+    private Response copyResponse(Response responseFromUib) {
+        Response.ResponseBuilder rb = Response.status(responseFromUib.getStatusInfo());
+        if (responseFromUib.hasEntity()) {
+            rb.entity(responseFromUib.getEntity());
+        }
+        return rb.build();
+    }
+
+    @GET
+    @Path("/{applicationId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getApplication(@PathParam("applicationtokenid") String applicationTokenId,
+                                   @PathParam("userTokenId") String userTokenId,
+                                   @PathParam("applicationId") String applicationId){
+        log.trace("getApplication is called with applicationId={}", applicationId);
+        WebTarget webResource = uib.path(applicationTokenId).path(userTokenId).path(APPLICATION_PATH).path(applicationId);
+        Response responseFromUib = webResource.request(MediaType.APPLICATION_JSON).get();
+        return copyResponse(responseFromUib);
+    }
+
+    @PUT
+    @Path("/{applicationId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateApplication(@PathParam("applicationtokenid") String applicationTokenId,
+                                      @PathParam("userTokenId") String userTokenId,
+                                      @PathParam("applicationId") String applicationId,
+                                      String applicationJson)  {
+        log.trace("updateApplication applicationId={}, applicationJson={}", applicationId, applicationJson);
+        WebTarget webResource = uib.path(applicationTokenId).path(userTokenId).path(APPLICATION_PATH).path(applicationId);
+        Response responseFromUib = webResource.request(MediaType.APPLICATION_JSON).put(Entity.entity(applicationJson, MediaType.APPLICATION_JSON));
+        return copyResponse(responseFromUib);
+    }
+
+    @DELETE
+    @Path("/{applicationId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteApplication(@PathParam("applicationtokenid") String applicationTokenId,
+                                      @PathParam("userTokenId") String userTokenId,
+                                      @PathParam("applicationId") String applicationId){
+        log.trace("deleteApplication is called with applicationId={}", applicationId);
+
+        WebTarget webResource = uib.path(applicationTokenId).path(userTokenId).path(APPLICATION_PATH).path(applicationId);
+        Response responseFromUib = webResource.request(MediaType.APPLICATION_JSON).delete();
+        return copyResponse(responseFromUib);
+    }
+
+
+
+    /*
+    //TODO: Enhance due to https://github.com/Cantara/Whydah-UserAdminService/issues/20
     @POST
     @Path("/")
     @Consumes(MediaType.APPLICATION_XML)
@@ -64,13 +131,7 @@ public class ApplicationResource {
         }
     }
 
-    /**
-     * TODO enchance due to https://github.com/Cantara/Whydah-UserAdminService/issues/20
-     * @param applicationTokenId
-     * @param userTokenId
-     * @param applicationId
-     * @return
-     */
+   //TODO enchance due to https://github.com/Cantara/Whydah-UserAdminService/issues/20
     @GET
     @Path("/{applicationId}")
     @Produces(MediaType.APPLICATION_XML)
@@ -92,6 +153,15 @@ public class ApplicationResource {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    protected String buildApplicationXml(Application application) {
+        String applicationCreatedXml = null;
+        if (application != null) {
+            applicationCreatedXml = application.toXML();
+        }
+        return applicationCreatedXml;
+    }
+    */
 
     /**
      * Enhance due to https://github.com/Cantara/Whydah-UserAdminService/issues/20
@@ -127,22 +197,5 @@ public class ApplicationResource {
     @Produces(MediaType.TEXT_HTML)
     public Response ping() {
         return Response.ok("pong").build();
-    }
-
-    protected String buildApplicationJson(Application application) {
-        String applicationCreatedJson = null;
-        try {
-            applicationCreatedJson = mapper.writeValueAsString(application);
-        } catch (IOException e) {
-            log.warn("Could not convert application to Json {}", application.toString());
-        }
-        return applicationCreatedJson;
-    }
-    protected String buildApplicationXml(Application application) {
-        String applicationCreatedXml = null;
-        if (application != null) {
-            applicationCreatedXml = application.toXML();
-        }
-        return applicationCreatedXml;
     }
 }
