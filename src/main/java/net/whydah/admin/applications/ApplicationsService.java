4f5fb9b611b401f54b2c7c0c08892e65190efe9e
huy <misterhuydo@gmail.com>
2017-02-13 17:19:35 +0700

    do not check access for systest user

--- src/main/java/net/whydah/admin/applications/ApplicationsService.java
+++ src/main/java/net/whydah/admin/applications/ApplicationsService.java
@@ -9,8 +9,11 @@ import net.whydah.sso.application.mappers.ApplicationMapper;
 import net.whydah.sso.commands.appauth.CommandGetApplicationIdFromApplicationTokenId;
 import net.whydah.sso.commands.appauth.CommandValidateApplicationTokenId;
 import net.whydah.sso.commands.appauth.CommandVerifyUASAccessByApplicationTokenId;
+import net.whydah.sso.commands.userauth.CommandGetUsertokenByUsertokenId;
 import net.whydah.sso.commands.userauth.CommandValidateUsertokenId;
 import net.whydah.sso.commands.userauth.CommandValidateWhydahAdminByUserTokenId;
+import net.whydah.sso.user.mappers.UserTokenMapper;
+import net.whydah.sso.user.types.UserToken;
 
 import org.constretto.annotation.Configuration;
 import org.constretto.annotation.Configure;
@@ -77,6 +80,8 @@ public class ApplicationsService {
     }
 
     public boolean hasAccess(String applicationTokenId, String userTokenId) {
+    	
+    	
     	Boolean userTokenIsValid = new CommandValidateUsertokenId(URI.create(stsUrl), applicationTokenId, userTokenId).execute();
     	if(!userTokenIsValid){
     		log.warn("CommandValidateUsertokenId failed, user token " + userTokenId + " is invalid");
@@ -87,16 +92,25 @@ public class ApplicationsService {
     		log.warn("CommandValidateApplicationTokenId failed, app token " + applicationTokenId + " is invalid");
     		return false;
     	}
+    	
+    	
     	Boolean userTokenIsAdmin = new CommandValidateWhydahAdminByUserTokenId(URI.create(stsUrl), applicationTokenId, userTokenId).execute();
     	if(!userTokenIsAdmin){
     		log.warn("CommandValidateWhydahAdminByUserTokenId failed, user token " + userTokenId + " does not have admin role");
     		return false;
+    	} else {
+        	String userTokenXml = new CommandGetUsertokenByUsertokenId(URI.create(stsUrl), applicationTokenId, null, userTokenId).execute();
+        	UserToken userToken = UserTokenMapper.fromUserTokenXml(userTokenXml);
+        	if(userToken.getUserName().equals("systest")){
+        		return true;
+        	}
     	}
     	Boolean isUASAccessOpen = new CommandVerifyUASAccessByApplicationTokenId(stsUrl, applicationTokenId).execute();
     	if(!isUASAccessOpen){
     		log.warn("CommandVerifyUASAccessByApplicationTokenId failed, app token " + applicationTokenId + " does not have UAS access");
     		return false;
     	}
+    	
     	return true;
     }
 
