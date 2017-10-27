923bb646a3ef0c20a6410bb82c992a83ca906547
totto <totto@totto.org>
2017-10-27 14:44:07 +0200

    excess paths in securityfilter

--- src/main/java/net/whydah/admin/security/SecurityFilter.java
+++ src/main/java/net/whydah/admin/security/SecurityFilter.java
@@ -139,12 +139,13 @@ public class SecurityFilter implements Filter {
         String userSignupPattern = "/signup/user";
         String listApplicationsPattern = "/applications";
         String findApplicationsPattern = "/applications/find/(.*?)";
+        String findApplicationsPattern_ = "/applications/find";
         String findApplicationsPattern2 = "/find/applications/(.*?)";
+        String findApplicationsPattern2_ = "/find/applications";
         String hasUASAccess = "/hasUASAccess";
         String send_scheduled_email = "/send_scheduled_email";
-
         String userPWEnabeled = "/user/.+/password_login_enabled";
-        String[] patternsWithoutUserTokenId = {applicationAuthPattern, userLogonPattern, pwResetAuthPattern, pwPattern, userAuthPattern, userSignupPattern, listApplicationsPattern, hasUASAccess, send_scheduled_email, userPWEnabeled, findApplicationsPattern, findApplicationsPattern2};
+        String[] patternsWithoutUserTokenId = {applicationAuthPattern, userLogonPattern, pwResetAuthPattern, pwPattern, userAuthPattern, userSignupPattern, listApplicationsPattern, hasUASAccess, send_scheduled_email, userPWEnabeled, findApplicationsPattern, findApplicationsPattern2, findApplicationsPattern_, findApplicationsPattern2_};
         for (String pattern : patternsWithoutUserTokenId) {
             if (Pattern.compile(pattern).matcher(path).matches()) {
                 log.debug("{} was matched to {}. SecurityFilter passed.", path, pattern);
