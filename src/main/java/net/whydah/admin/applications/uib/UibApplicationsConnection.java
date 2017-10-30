83c814e02f12a33d078ae31b040a471ab857868b
totto <totto@totto.org>
2017-10-30 22:10:19 +0100

    more debug statements for locating issues

--- src/main/java/net/whydah/admin/applications/uib/UibApplicationsConnection.java
+++ src/main/java/net/whydah/admin/applications/uib/UibApplicationsConnection.java
@@ -3,6 +3,7 @@ package net.whydah.admin.applications.uib;
 import net.whydah.admin.errorhandling.AppException;
 import net.whydah.admin.errorhandling.AppExceptionCode;
 import net.whydah.admin.security.UASCredentials;
+import net.whydah.sso.application.types.ApplicationTokenID;
 import org.constretto.annotation.Configuration;
 import org.constretto.annotation.Configure;
 import org.slf4j.Logger;
@@ -34,7 +35,7 @@ public class UibApplicationsConnection {
     private final UASCredentials uasCredentials;
     private static String cachedApplicationsString = "";
     private static Instant cachedApplicationsStringInstant;
-    private static Map<String, String> cachedApplicationMap = new LinkedHashMap();
+    private static Map<String, String> cachedApplicationMap = new LinkedHashMap<String, String>();
     private static Instant cachedApplicationMapInstant;
 
     @Autowired
@@ -97,56 +98,66 @@ public class UibApplicationsConnection {
     }
 
     public String findApplications(String applicationTokenId, String userTokenId, String query) throws AppException {
-        if (applicationTokenId == null || applicationTokenId.length() < 4 || query == null || query.length() < 1) {
-            log.warn("findApplications - Null or bogus applicationTokenId found {} query:{}, returning null", applicationTokenId, query);
-            return null;  // DO NOT BLOCK THREAD on requests that are doomed to fail
-        }
-        if (cachedApplicationMap.get(query) != null) {
-            if (Instant.now().isBefore(cachedApplicationMapInstant.plusSeconds(20))) {
-                log.info("findApplications - Returning application(s) from cache");
-                // 30 second cache to avoid too much UIB noise
-                return cachedApplicationMap.get(query);
+        try {
+            if (!ApplicationTokenID.isValid(applicationTokenId) || query == null) {
+                log.warn("findApplications - Null or bogus applicationTokenId found {} query:{}, returning null", applicationTokenId, query);
+                return null;  // DO NOT BLOCK THREAD on requests that are doomed to fail
             }
-        }
+            if (query.length() < 1) {
+                log.warn("findApplications - Empty query found {} query:{}, returning null", applicationTokenId, query);
+                return null;  // DO NOT BLOCK THREAD on requests that are doomed to fail
+            }
+            if (cachedApplicationMap.get(query) != null) {
+                if (Instant.now().isBefore(cachedApplicationMapInstant.plusSeconds(20))) {
+                    log.info("findApplications - Returning application(s) from cache");
+                    // 30 second cache to avoid too much UIB noise
+                    return cachedApplicationMap.get(query);
+                }
+            }
 
 //        Client client = ClientBuilder.newClient();
-        log.info("findApplications - Connection to UserIdentityBackend on {}", userIdentityBackendUri);
+            log.info("findApplications - Connection to UserIdentityBackend on {}", userIdentityBackendUri);
 //        uib = client.target(userIdentityBackendUri);
-        //WebTarget webResource = uib.path("/" + userAdminServiceTokenId + "/applications/find/"+query);
+            //WebTarget webResource = uib.path("/" + userAdminServiceTokenId + "/applications/find/"+query);
 //        WebTarget webResource = uib.path(applicationTokenId).path(userTokenId).path("applications").path("find").path(query);
-        //WebTarget webResource = uib.path("/applications/find/"+query);
+            //WebTarget webResource = uib.path("/applications/find/"+query);
 //        Response response = webResource.request(MediaType.APPLICATION_JSON).header(UASCredentials.APPLICATION_CREDENTIALS_HEADER_XML, uasCredentials.getApplicationCredentialsXmlEncoded()).get();
-        // String output = response.readEntity(String.class);
+            // String output = response.readEntity(String.class);
 
-        Response response =
-                new CommandFindApplicationsFromUIB(userIdentityBackendUri, applicationTokenId, userTokenId, uasCredentials.getApplicationCredentialsXmlEncoded(), query).execute();
+            Response response =
+                    new CommandFindApplicationsFromUIB(userIdentityBackendUri, applicationTokenId, userTokenId, uasCredentials.getApplicationCredentialsXmlEncoded(), query).execute();
 
-        int statusCode = response.getStatus();
-        String output = response.readEntity(String.class);
-        log.info("findApplications {}", first50(output));
-        switch (statusCode) {
-            case STATUS_OK:
-                log.debug("findApplications-Response from UIB: {}: {}", statusCode, output);
-                cachedApplicationMapInstant = Instant.now();
-                cachedApplicationMap = new LinkedHashMap();  // only old data, start caching again
-                cachedApplicationMap.put(query, output);
-                break;
-            case NO_CONTENT:
-                break;
-            case STATUS_BAD_REQUEST:
-                log.error("findApplications-Response from UIB: {}: {}", statusCode, output);
-                //throw new BadRequestException("listAll failed. Bad request " + response.toString() + ",  Status code " + response.getStatus());
-                throw AppExceptionCode.MISC_BadRequestException_9997.setDeveloperMessage("findApplications-Response from UIB: {}: {}", response.getStatus(), output);
-            case NOT_AUTHERIZED:
-                log.error("findApplications-Response from UIB: {}: {}", statusCode, output);
-                //throw new BadRequestException("listAll failed. Bad request " + response.toString() + ",  Status code " + response.getStatus());
-                throw AppExceptionCode.MISC_BadRequestException_9997.setDeveloperMessage("findApplications-Response from UIB: {}: {}", response.getStatus(), output);
-            default:
-                log.error("findApplications-Response from UIB: {}: {}", statusCode, output);
-                //throw new AuthenticationFailedException("listAll failed. Status code " + response.getStatus());
-                throw AppExceptionCode.MISC_OperationFailedException_9996.setDeveloperMessage("findApplications-Response from UIB: {}: {}", response.getStatus(), output);
+            int statusCode = response.getStatus();
+            String output = response.readEntity(String.class);
+            log.info("findApplications {}", first50(output));
+            switch (statusCode) {
+                case STATUS_OK:
+                    log.debug("findApplications-Response from UIB: {}: {}", statusCode, output);
+                    cachedApplicationMapInstant = Instant.now();
+                    cachedApplicationMap = new LinkedHashMap<String, String>();  // only old data, start caching again
+                    cachedApplicationMap.put(query, output);
+                    break;
+                case NO_CONTENT:
+                    break;
+                case STATUS_BAD_REQUEST:
+                    log.error("findApplications-Response from UIB: {}: {}", statusCode, output);
+                    //throw new BadRequestException("listAll failed. Bad request " + response.toString() + ",  Status code " + response.getStatus());
+                    throw AppExceptionCode.MISC_BadRequestException_9997.setDeveloperMessage("findApplications-Response from UIB: {}: {}", response.getStatus(), output);
+                case NOT_AUTHERIZED:
+                    log.error("findApplications-Response from UIB: {}: {}", statusCode, output);
+                    //throw new BadRequestException("listAll failed. Bad request " + response.toString() + ",  Status code " + response.getStatus());
+                    throw AppExceptionCode.MISC_BadRequestException_9997.setDeveloperMessage("findApplications-Response from UIB: {}: {}", response.getStatus(), output);
+                default:
+                    log.error("findApplications-Response from UIB: {}: {}", statusCode, output);
+                    //throw new AuthenticationFailedException("listAll failed. Status code " + response.getStatus());
+                    throw AppExceptionCode.MISC_OperationFailedException_9996.setDeveloperMessage("findApplications-Response from UIB: {}: {}", response.getStatus(), output);
+            }
+            return output;
+        } catch (Exception e) {
+            log.error("findApplications-Exception in UIB request, returning null", e);
+
         }
-        return output;
+        return null;
     }
 
     public static void clearCache() {
