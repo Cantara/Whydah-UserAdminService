cbeb88e6c669113c5b9e306b1cfb0128d0f22eb3
totto <totto@totto.org>
2020-05-26 12:01:11 +0200

    reverted

--- src/main/java/net/whydah/admin/email/MailSender.java
+++ src/main/java/net/whydah/admin/email/MailSender.java
@@ -78,10 +78,10 @@ public class MailSender {
 		smtpProperties.put("mail.smtp.host", smtpHost);
 		smtpProperties.put("mail.smtp.socketFactory.port", smtpPort);
 		smtpProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		smtpProperties.put("mail.smtp.socketFactory.fallback", "false");
-		smtpProperties.put("mail.smtp.auth.mechanisms", "XOAUTH2");
+//		smtpProperties.put("mail.smtp.socketFactory.fallback", "false");
+//		smtpProperties.put("mail.smtp.auth.mechanisms", "XOAUTH2");
 		smtpProperties.put("mail.smtp.auth", "true");
-		smtpProperties.put("mail.smtp.starttls.enable", "true");
+//		smtpProperties.put("mail.smtp.starttls.enable", "true");
 		smtpProperties.put("mail.smtp.port", smtpPort);
 		smtpProperties.put("mail.debug", "true");
 
