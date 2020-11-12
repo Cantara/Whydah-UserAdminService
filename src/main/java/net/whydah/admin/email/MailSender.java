c0b1c7d0c210a171fd1c472c375e361fdbcab078
totto <totto@totto.org>
2020-11-12 11:39:56 +0100

    removing noisy logging

--- src/main/java/net/whydah/admin/email/MailSender.java
+++ src/main/java/net/whydah/admin/email/MailSender.java
@@ -60,11 +60,11 @@ public class MailSender {
 		log.info("email.smtp.from.address:" + smtpFromAddress);
 		log.info("email.smtp.from.personalname:" + smtpFromPersonalname);
 
-		try {
-			send("totto@totto.org", "Testmail fra Whydah", "mostly empty body");
-		} catch (Exception e) {
-			log.warn("Trouble with testmail:", e);
-		}
+//		try {
+//			send("totto@totto.org", "Testmail fra Whydah", "mostly empty body");
+//		} catch (Exception e) {
+//			log.warn("Trouble with testmail:", e);
+//		}
 
 		// https://accounts.google.com/DisplayUnlockCaptcha
 	}
