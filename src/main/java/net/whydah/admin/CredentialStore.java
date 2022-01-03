5b1b3033ee4e5e5dbf57db277863e84dece07232
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2022-01-03 14:17:50 +0100

    Made CredentialStore Thread safe (must still fix WAS2). Use LRU caches.

--- src/main/java/net/whydah/admin/CredentialStore.java
+++ src/main/java/net/whydah/admin/CredentialStore.java
@@ -4,19 +4,17 @@ import net.whydah.sso.application.types.ApplicationCredential;
 import net.whydah.sso.commands.appauth.CommandGetApplicationIdFromApplicationTokenId;
 import net.whydah.sso.commands.appauth.CommandValidateApplicationTokenId;
 import net.whydah.sso.ddd.model.application.ApplicationId;
-import net.whydah.sso.session.WhydahApplicationSession;
 import net.whydah.sso.session.WhydahApplicationSession2;
-
 import org.constretto.annotation.Configuration;
 import org.constretto.annotation.Configure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author <a href="bard.lind@gmail.com">Bard Lind</a>
@@ -24,13 +22,54 @@ import java.util.Set;
 @Repository
 public class CredentialStore {
 
-    private static WhydahApplicationSession2 whydahApplicationSession = null;
-    private final String stsUri;
-    private final String uasUri;
-    private final ApplicationCredential uasApplicationCredential;
-    private static Set okApplicationTokenSet = new LinkedHashSet<String>();
-    private static Map<String, String> okApplicationIDMap = new LinkedHashMap<String, String>();
+    public static final int OK_APPLICATION_TOKEN_SET_MAX_SIZE = 30;
+    public static final int OK_APPLICATION_TOKEN_ID_MAP_MAX_SIZE = 50;
 
+    private static class WhydahApplicationSession2Singleton {
+        private final static WhydahApplicationSession2 whydahApplicationSession;
+
+        static {
+            WAS2Configuration was2Conf = was2ConfigurationRef.get();
+            if (was2Conf == null) {
+                throw new IllegalStateException("No WAS2Configuration available for initialization of WAS2");
+            }
+            whydahApplicationSession = WhydahApplicationSession2.getInstance(was2Conf.stsUri, was2Conf.uasUri, was2Conf.uasApplicationCredential);
+        }
+
+        public static WhydahApplicationSession2 getSession() {
+            return whydahApplicationSession;
+        }
+    }
+
+    private static class WAS2Configuration {
+        private final String stsUri;
+        private final String uasUri;
+        private final ApplicationCredential uasApplicationCredential;
+
+        private WAS2Configuration(String stsUri, String uasUri, ApplicationCredential uasApplicationCredential) {
+            this.stsUri = stsUri;
+            this.uasUri = uasUri;
+            this.uasApplicationCredential = uasApplicationCredential;
+        }
+    }
+
+    private static final AtomicReference<WAS2Configuration> was2ConfigurationRef = new AtomicReference<>();
+
+    private static Map<String, String> okApplicationTokenSet = Collections.synchronizedMap(new LinkedHashMap<String, String>() {
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<String, String> eldest) {
+            return size() > OK_APPLICATION_TOKEN_SET_MAX_SIZE;
+        }
+    });
+
+    private static Map<String, String> okApplicationIDMap = Collections.synchronizedMap(new LinkedHashMap<String, String>() {
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<String, String> eldest) {
+            return size() > OK_APPLICATION_TOKEN_ID_MAP_MAX_SIZE;
+        }
+    });
+
+    private final String myApplicationId;
 
     @Autowired
     @Configure
@@ -39,22 +78,18 @@ public class CredentialStore {
                            @Configuration("applicationid") String applicationid,
                            @Configuration("applicationname") String applicationname,
                            @Configuration("applicationsecret") String applicationsecret) {
-        this.stsUri = stsUri;
-        this.uasUri = uasUri;
-        this.uasApplicationCredential = new ApplicationCredential(applicationid, applicationname, applicationsecret);
-        getWas();//Init WAS first
+        this.myApplicationId = applicationid;
+        ApplicationCredential uasApplicationCredential = new ApplicationCredential(applicationid, applicationname, applicationsecret);
+        was2ConfigurationRef.set(new WAS2Configuration(stsUri, uasUri, uasApplicationCredential));
+        WhydahApplicationSession2Singleton.getSession(); // force WAS2 initialization
     }
 
     public String getMyApplicationID() {
-        return uasApplicationCredential.getApplicationID();
+        return myApplicationId;
     }
 
     public String getUserAdminServiceTokenId() {
-        if (whydahApplicationSession == null) {
-            getWas();
-        }
         return getWas().getActiveApplicationTokenId();
-
     }
 
     public boolean hasValidApplicationSession() {
@@ -62,45 +97,34 @@ public class CredentialStore {
     }
 
     public boolean isValidApplicationSession(URI tokenServiceUri, String applicationTokenId) {
-        if (okApplicationTokenSet.size() > 30) { // small size, no timeout yet
-            okApplicationTokenSet = new LinkedHashSet<String>();
-        }
-        if (okApplicationTokenSet.contains(applicationTokenId)) {
+        String emptyString = okApplicationTokenSet.remove(applicationTokenId); // remove to ensure that this item will be the most recently seen
+        if (emptyString != null) {
+            okApplicationTokenSet.put(applicationTokenId, "");
             return true;
         }
         boolean isOk = new CommandValidateApplicationTokenId(tokenServiceUri, getWas().getActiveApplicationTokenId()).execute();
         if (isOk) {
-            okApplicationTokenSet.add(applicationTokenId);
+            okApplicationTokenSet.put(applicationTokenId, "");
         }
         return isOk;
-
     }
 
     public String getApplicationID(String callerApplicationTokenId) {
-        if (okApplicationIDMap.size() > 50) { // small size, no timeout
-            okApplicationIDMap = new LinkedHashMap<String, String>();
+        String appId = okApplicationIDMap.remove(callerApplicationTokenId); // remove to ensure that this item will be the most recently seen
+        if (appId != null) {
+            okApplicationIDMap.put(callerApplicationTokenId, appId); // most-recent
+            return appId;
         }
-        if (okApplicationIDMap.containsKey(callerApplicationTokenId)) {
-            return okApplicationIDMap.get(callerApplicationTokenId);
-        }
-        String appId = new CommandGetApplicationIdFromApplicationTokenId(URI.create(getWas().getSTS()), callerApplicationTokenId).execute();
+        appId = new CommandGetApplicationIdFromApplicationTokenId(URI.create(getWas().getSTS()), callerApplicationTokenId).execute();
         //HUY: it is a bug, appid can be null for the first time. we should only add the value to the list if it is something not a null value
-        if (appId!=null && ApplicationId.isValid(appId)) {
+        if (appId != null && ApplicationId.isValid(appId)) {
             okApplicationIDMap.put(callerApplicationTokenId, appId);
             return appId;
         }
         return null;
-
     }
 
     public WhydahApplicationSession2 getWas() {
-        if (whydahApplicationSession == null) {
-            setWas(WhydahApplicationSession2.getInstance(stsUri, uasUri, uasApplicationCredential));
-        }
-        return whydahApplicationSession;
-    }
-
-    public synchronized void setWas(WhydahApplicationSession2 was) {
-        whydahApplicationSession = was;
+        return WhydahApplicationSession2Singleton.getSession();
     }
 }
