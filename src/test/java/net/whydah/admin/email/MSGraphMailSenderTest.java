d28845db72e7e91ee039c6e1c46e67a76289c7d8
huy <huy@DESKTOP-TRTHL1N>
2022-03-11 17:19:54 +0700

    prepare to deploy

--- src/test/java/net/whydah/admin/email/MSGraphMailSenderTest.java
+++ src/test/java/net/whydah/admin/email/MSGraphMailSenderTest.java
@@ -13,19 +13,12 @@ public class MSGraphMailSenderTest {
 	@Test
 	private void testSending() {
 		
-		MsGraphMailSender sender = new MsGraphMailSender("sso@1881.no", 
-				"cceabdd8-db03-48f7-9c5e-741a7b80a825",
-				"d7502a07-6b95-4a34-81f4-67d75021c5f4", 
-				"MtF7Q~FwgLVmh-.hAg9MHVDoyxfd6.XPmoDtx"
-				);
-		sender.send("misterhuydo@gmail.com", "test from Azure", "test test test");
-//		
-//		MsGraphMailSender sender = new MsGraphMailSender("misterhuydo@gmail.com", 
-//				"a1882812-cbbf-4c7f-8e19-caa5f80a26cb", 
-//				"8ee9f573-4e80-443a-87bc-74df80157fec", 
-//				"8rB7Q~-dd3sB5DPSHcUNeYxd17q8KqP11jf.8"
+//		MsGraphMailSender sender = new MsGraphMailSender("sso@1881.no", 
+//				"cceabdd8-db03-48f7-9c5e-741a7b80a825",
+//				"d7502a07-6b95-4a34-81f4-67d75021c5f4", 
+//				"Z727Q~cxzuloSP57dY.ltVIqghS2eTWwyAXK."
 //				);
-//		sender.send("goodemailtokeep@gmail.com", "test from Azure", "test test test");
+//		sender.send("misterhuydo@gmail.com", "test from Azure", "test test test");
 	
 	}
 	
