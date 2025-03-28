d9a08aa0c418779f7b3e22164fb7d9f481fa9c85
huy <huy@DESKTOP-99E4IJQ>
2025-03-28 21:57:40 +0700

    disable Hk2

--- src/main/java/net/whydah/admin/config/JerseyApplication.java
+++ src/main/java/net/whydah/admin/config/JerseyApplication.java
@@ -11,12 +11,26 @@ public class JerseyApplication extends ResourceConfig {
     private static final Logger log = LoggerFactory.getLogger(JerseyApplication.class);
 
     public JerseyApplication() {
+    
         // Register package for scanning
         packages("net.whydah.admin");
 
         // Register Spring integration
         register(RequestContextFilter.class);
+        
+        property("jersey.config.server.disableHk2", true);
 
+        log.info("SpringComponentProvider present: {}", isClassPresent("org.glassfish.jersey.ext.spring6.SpringComponentProvider"));
+        log.info("Registered classes: {}", getClasses());
         log.debug(this.getClass().getSimpleName() + " started!");
+    }
+
+    private boolean isClassPresent(String className) {
+        try {
+            Class.forName(className);
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
     }
 }
