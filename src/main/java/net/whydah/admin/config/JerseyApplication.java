2e82540d7d48fe77a4093dcde09cbbf26d8fa8ac
huy <huy@DESKTOP-99E4IJQ>
2025-03-28 21:39:10 +0700

    minor

--- src/main/java/net/whydah/admin/config/JerseyApplication.java
+++ src/main/java/net/whydah/admin/config/JerseyApplication.java
@@ -2,9 +2,9 @@ package net.whydah.admin.config;
 
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 @ApplicationPath("overriddenByWebXml")
 public class JerseyApplication extends ResourceConfig {
@@ -14,18 +14,8 @@ public class JerseyApplication extends ResourceConfig {
         // Register package for scanning
         packages("net.whydah.admin");
 
-        // Do NOT disable auto-discovery when using HK2
-        // property("jersey.config.disableAutoDiscovery", true);
-
         // Register Spring integration
-        try {
-            // Try to register the Spring 6 integration
-            Class<?> filterClass = Class.forName("org.glassfish.jersey.ext.spring6.SpringComponentProvider");
-            log.info("Found Spring 6 integration: {}", filterClass.getName());
-            register(org.glassfish.jersey.server.spring.scope.RequestContextFilter.class);
-        } catch (ClassNotFoundException e) {
-            log.error("Spring 6 integration not found! Dependency may be missing", e);
-        }
+        register(RequestContextFilter.class);
 
         log.debug(this.getClass().getSimpleName() + " started!");
     }
