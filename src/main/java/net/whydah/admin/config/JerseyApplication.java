1e5209cfd0d00eae5a0b32e764f80e3623df5720
totto <totto@totto.org>
2025-04-06 14:50:09 +0200

    try to get the wiring working

--- src/main/java/net/whydah/admin/config/JerseyApplication.java
+++ src/main/java/net/whydah/admin/config/JerseyApplication.java
@@ -1,35 +1,43 @@
 package net.whydah.admin.config;
+
+import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import jakarta.servlet.ServletContext;
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Context;
 
 @ApplicationPath("overriddenByWebXml")
 public class JerseyApplication extends ResourceConfig {
     private static final Logger log = LoggerFactory.getLogger(JerseyApplication.class);
 
-    @Context
-    private ServletContext servletContext;
-
-    
     public JerseyApplication() {
-    
-    	packages("net.whydah.admin");
+        // Register package for scanning
+        packages("net.whydah.admin");
 
-    	// Register Spring integration
-    	register(RequestContextFilter.class);
-    	register(org.glassfish.jersey.server.spring.SpringComponentProvider.class);
-    	register(JerseyBindings.class);
-     
-        log.info("Registered classes: {}", getClasses());
-    	log.debug(this.getClass().getSimpleName() + " started!");
+        // Do NOT disable auto-discovery when using HK2
+        // property("jersey.config.disableAutoDiscovery", true);
 
-        
+        // Register Spring integration
+        // Register Spring integration
+        try {
+            // Try to register the Spring integration using the correct package structure
+            // Jersey Spring integration is handled automatically as long as jersey-spring6 is on the classpath
+            log.info("Using Jersey Spring 6 integration from jersey-spring6 library");
+
+            // Explicitly register the filter (if needed)
+            register(org.glassfish.jersey.server.spring.scope.RequestContextFilter.class);
+        } catch (Throwable e) {
+            log.error("Spring 6 integration failed! Check your dependencies", e);
+        }
+//        try {
+//            // Try to register the Spring 6 integration
+//            Class<?> filterClass = Class.forName("org.glassfish.jersey.ext.spring6.SpringComponentProvider");
+//            log.info("Found Spring 6 integration: {}", filterClass.getName());
+//            register(org.glassfish.jersey.server.spring.scope.RequestContextFilter.class);
+//        } catch (ClassNotFoundException e) {
+//            log.error("Spring 6 integration not found! Dependency may be missing", e);
+//        }
+
+        log.debug(this.getClass().getSimpleName() + " started!");
     }
-   
-    
 }
